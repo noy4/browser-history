885e0fca78a56930477ff430fbf639492bb794ac
noy4 <naoyan418@gmail.com>
2025-02-05 18:45:38 +0900

    remove createNote

--- src/browser-history.ts
+++ src/browser-history.ts
@@ -1,6 +1,6 @@
 import type { App, TFile } from 'obsidian'
 import type BrowserHistoryPlugin from './main'
-import { addDays, addMonths, format, startOfDay, startOfMonth, subDays, subMonths } from 'date-fns'
+import { addDays, format, startOfDay, subDays } from 'date-fns'
 import { Notice } from 'obsidian'
 import { DBClient } from './db'
 
@@ -27,25 +27,6 @@ export class BrowserHistory {
     this.db = await DBClient.load({
       sqlitePath: this.plugin.settings.sqlitePath,
     })
-  }
-
-  async createNotes() {
-    const thisMonth = startOfMonth(new Date())
-    const settingFromDate = this.plugin.settings.fromDate
-      ? new Date(`${this.plugin.settings.fromDate} 00:00:00`)
-      : thisMonth
-    const monthDiff = thisMonth.getMonth() - settingFromDate.getMonth()
-
-    const fromToDates = Array.from({ length: monthDiff + 1 })
-      .map((_, i) => {
-        const fromDate = subMonths(thisMonth, i)
-        const toDate = addMonths(fromDate, 1)
-        return [fromDate, toDate]
-      })
-
-    for (const [fromDate, toDate] of fromToDates) {
-      // await this.createNote({ fromDate, toDate, skipIfExists: true })
-    }
   }
 
   async createDailyNotes() {
@@ -126,56 +107,4 @@ export class BrowserHistory {
     else
       await this.app.vault.modify(file as TFile, data)
   }
-
-  // createNote(params?: CreateNoteParams) {
-  //   try {
-  //     return this._createNote(params)
-  //   }
-  //   catch (e) {
-  //     new Notice(`[Browser History] ${e}`)
-  //   }
-  // }
-
-  // async _createNote(params?: CreateNoteParams) {
-  //   let { fromDate, toDate, skipIfExists } = params || {}
-  //   fromDate ||= startOfMonth(new Date())
-
-  //   const title = format(fromDate, 'yyyy-MM')
-  //   const path = [this.plugin.settings.folderPath, `${title}.md`].join('/')
-
-  //   if (skipIfExists && this.app.vault.getAbstractFileByPath(path)) {
-  //     log(`skip creating note: ${path}`)
-  //     return
-  //   }
-
-  //   const records = this.db.getUrls({ fromDate, toDate })
-
-  //   if (records.length === 0) {
-  //     log(`no records found for ${title}`)
-  //     return
-  //   }
-
-  //   const dayMap = new Map<number, Record<string, SqlValue>[]>()
-  //   for (const row of records) {
-  //     const { last_visit_time } = row
-  //     const date = new Date(Number(last_visit_time))
-  //     const day = startOfDay(date).getTime()
-  //     const dayMapValue = dayMap.get(day) || []
-  //     dayMapValue.push(row)
-  //     dayMap.set(day, dayMapValue)
-  //   }
-
-  //   const content = [...dayMap].map(([day, rows]) => {
-  //     const formattedDate = format(new Date(Number(day)), 'M/d')
-  //     const formattedRows = rows.map((row) => {
-  //       const { title, url, last_visit_time } = row
-  //       const timestamp = format(new Date(Number(last_visit_time)), 'HH:mm')
-  //       return `- ${timestamp} [${title}](${url})`
-  //     })
-  //     return `## ${formattedDate}\n${formattedRows.join('\n')}`
-  //   }).join('\n\n')
-
-  //   await this.upsertFile(path, content)
-  //   new Notice('Browser history note created!')
-  // }
 }
