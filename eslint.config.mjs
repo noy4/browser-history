e01f3003a1b4a7dc4ba53e3e13936907620a63f4
noy4 <naoyan418@gmail.com>
2025-02-07 21:02:33 +0900

    md trailing spade

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,17 @@ export default antfu({
     'no-console': 'off', // allow console.log()
     'no-new': 'off', // allow new Notice()
   },
-})
+}, md())
+
+/**
+ * markdown config
+ * @return {import('@antfu/eslint-config').TypedFlatConfigItem}
+ */
+function md() {
+  return {
+    files: ['**/*.md'],
+    rules: {
+      'style/no-trailing-spaces': 'off',
+    },
+  }
+}
